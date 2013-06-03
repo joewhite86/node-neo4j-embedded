f6d347ac50f24ebd89040ff3131c22d3e549074a
Jochen Weis <jochen.weis@ecommerce-factory.ag>
2013-06-03 22:01:08 +0200

    updated to neo4j 1.9 and java 7 added wrapped ha option fixed some performance issues

--- java-src/Neo4jWrapper.java
+++ java-src/Neo4jWrapper.java
@@ -6,6 +6,7 @@ import org.neo4j.graphdb.Relationship;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 import org.neo4j.graphdb.factory.HighlyAvailableGraphDatabaseFactory;
 import org.neo4j.kernel.GraphDatabaseAPI;
+import org.neo4j.kernel.HighlyAvailableGraphDatabase;
 import org.neo4j.server.WrappingNeoServerBootstrapper;
 import org.neo4j.server.configuration.ServerConfigurator;
 
@@ -40,11 +41,16 @@ public class Neo4jWrapper {
     this.installShutdownHook(graphDb);
     return graphDb;
   }
-  public GraphDatabaseService connectHA(String dir, Map<String, String> haConfig, Map<String, Object> serverProperties) {
-    GraphDatabaseAPI graphDb = (GraphDatabaseAPI) new HighlyAvailableGraphDatabaseFactory()
-      .newHighlyAvailableDatabaseBuilder(dir)
-      .setConfig(haConfig)
-      .newGraphDatabase();
+  public GraphDatabaseService connectHA(String dir, Map<String, String> haConfig) {
+    GraphDatabaseService graphDb = new HighlyAvailableGraphDatabaseFactory()
+        .newHighlyAvailableDatabaseBuilder(dir)
+        .setConfig(haConfig)
+        .newGraphDatabase();
+    this.installShutdownHook(graphDb);
+    return graphDb;
+  }
+  public GraphDatabaseService connectHAWrapped(String dir, Map<String, String> haConfig, Map<String, Object> serverProperties) {
+    GraphDatabaseAPI graphDb = (GraphDatabaseAPI) this.connectHA(dir, haConfig);
 
     ServerConfigurator config = new ServerConfigurator( graphDb );
     Iterator<String> iterator = serverProperties.keySet().iterator();
@@ -52,8 +58,6 @@ public class Neo4jWrapper {
       String key = iterator.next();
       config.configuration().setProperty(key, serverProperties.get(key));
     }
-
-    this.installShutdownHook(graphDb);
 
     new WrappingNeoServerBootstrapper( graphDb, config ).start();
 
@@ -86,7 +90,7 @@ public class Neo4jWrapper {
   }
 
   public Property[] getNodeProperties(Node node) {
-    ArrayList<Property> properties = new ArrayList<Property>();
+    ArrayList<Property> properties = new ArrayList<>();
     Iterator<String> iterator = node.getPropertyKeys().iterator();
     while(iterator.hasNext()) {
       String key = iterator.next();
@@ -98,14 +102,14 @@ public class Neo4jWrapper {
     return relationship.getType().name();
   }
   public Result query(GraphDatabaseService graphDb, String query, Map<String, Object> params) {
-    ArrayList<Object[]> results = new ArrayList<Object[]>();
-    ArrayList<String> columnNames = new ArrayList<String>();
+    ArrayList<Object[]> results = new ArrayList<>();
+    ArrayList<String> columnNames = new ArrayList<>();
     ExecutionEngine engine = new ExecutionEngine(graphDb);
     ExecutionResult result = engine.execute(query, params);
 
     Boolean firstRow = true;
     for(Map<String, Object> row : result) {
-      ArrayList<Object> rowResult = new ArrayList<Object>();
+      ArrayList<Object> rowResult = new ArrayList<>();
       for(Map.Entry<String, Object> column : row.entrySet()) {
         if(firstRow) columnNames.add(column.getKey());
         rowResult.add(column.getValue());
@@ -121,18 +125,20 @@ public class Neo4jWrapper {
   }
 
   public static void main(String[] args) {
-    HashMap<String, String> haConfig = new HashMap<String, String>();
+    Neo4jWrapper wrapper = new Neo4jWrapper();
+    HashMap<String, String> haConfig = new HashMap<>();
+    HashMap<String, Object> serverProperties = new HashMap<>();
 
     haConfig.put("ha.server_id", "3");
-    haConfig.put("ha.initial_hosts", ":5001,:5002,:5003");
+    haConfig.put("ha.initial_hosts", ":5001");
     haConfig.put("ha.server", ":6003");
     haConfig.put("ha.cluster_server", ":5003");
     haConfig.put("org.neo4j.server.database.mode", "HA");
 
-    final GraphDatabaseService graphDb = new HighlyAvailableGraphDatabaseFactory()
-      .newHighlyAvailableDatabaseBuilder("test.db")
-      .setConfig(haConfig)
-      .newGraphDatabase();
+    serverProperties.put("org.neo4j.server.webserver.port", "7676");
+    serverProperties.put("org.neo4j.server.webserver.https.port", "7675");
+
+    final GraphDatabaseService graphDb = wrapper.connectHA("test.db", haConfig);
 
     Runtime.getRuntime().addShutdownHook(new Thread() {
       @Override
